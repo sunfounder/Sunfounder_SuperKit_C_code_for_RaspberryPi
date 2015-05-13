dc6125f1320c183e31abd9c78d1b3eb4cc286431
sunfounder <chuanmorison@gmail.com>
2015-05-13 00:10:50 -0700

    add some author message

--- 02_BtnAndLed/BtnAndLed.c
+++ 02_BtnAndLed/BtnAndLed.c
@@ -1,3 +1,11 @@
+/**********************************************************************
+* Filename    : BtnAndLed.c
+* Description : Controlling an led by button.
+* Author      : Robot
+* E-mail      : support@sunfounder.com
+* website     : www.sunfounder.com
+* Date        : 2014/08/27
+**********************************************************************/
 #include <wiringPi.h>
 #include <stdio.h>
 
